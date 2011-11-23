e7e7ca43ec834fbf0014ac2a686e52352800aabf
Michael Schmitz <michael@schmitztech.com>
2011-11-22 21:41:13 -0800

    added normalizations to the lemmatize method (remove punctuation, trim)

--- src/main/scala/tool/stem/Stemmer.scala
+++ src/main/scala/tool/stem/Stemmer.scala
@@ -6,11 +6,16 @@ import common.main.LineProcessor
 
 abstract class Stemmer {
   def stem(word: String): String
-  def lemmatize(word: String) = this.stem(word).toLowerCase
+  def normalize(word: String) = Stemmer.remove.replaceAllIn(word.trim.toLowerCase, "")
+  def lemmatize(word: String) = this.stem(this.normalize(word))
+}
+
+object Stemmer {
+  val remove = """[()\[\].,;:"']""".r;
 }
 
 abstract class StemmerMain
 extends LineProcessor {
   def stemmer: Stemmer
-  override def process(line: String) = line.split("\\s+").map(stemmer.stem(_)).mkString(" ")
+  override def process(line: String) = line.split("\\s+").map(stemmer.lemmatize(_)).mkString(" ")
 }
