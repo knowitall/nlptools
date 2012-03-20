30e6ad904a005169f864d0f29bde3c748c1da5da
Michael Schmitz <michael@schmitztech.com>
2012-03-19 22:47:01 -0700

    Reorganized nlptools layout.  Broke it into many subprojects.  Upticked version to 2.0.0-SNAPSHOT.

---
+++ core/src/test/scala/edu/washington/cs/knowitall/tool/parse/pattern/PatternSpec.scala
@@ -0,0 +1,63 @@
+package edu.washington.cs.knowitall
+package tool
+package parse
+package pattern
+
+import graph._
+import org.specs._
+import org.specs.runner._
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnitSuiteRunner])
+class PatternSpecTest extends JUnit4(PatternSpec)
+object PatternSpec extends Specification {
+  def testMultipathPatternApplication = {
+    val pattern = DependencyPattern.deserialize("{arg1} <nsubjpass< {rel:postag=VBN} >prep_in> {arg2}")
+    val dgraph = DependencyGraph.deserialize("prep_in(mean_VBP_7, Canada_NNP_1); det(conditions_NNS_6, the_DT_2); amod(conditions_NNS_6, geographic_JJ_3); conj_and(geographic_JJ_3, climatic_JJ_5); amod(conditions_NNS_6, climatic_JJ_5); nsubj(mean_VBP_7, conditions_NNS_6); complm(established_VBN_12, that_IN_8); nsubjpass(established_VBN_12, LBAM_NNP_9); aux(established_VBN_12, could_MD_10); auxpass(established_VBN_12, become_VB_11); ccomp(mean_VBP_7, established_VBN_12); prep_in(established_VBN_12, parts_NNS_14); nn(Columbia_NNP_19, south_NN_16); amod(Columbia_NNP_19, coastal_JJ_17); nn(Columbia_NNP_19, British_NNP_18); prep_of(parts_NNS_14, Columbia_NNP_19); nn(Island_NNP_22, Vancouver_NNP_21); prep_of(parts_NNS_14, Island_NNP_22); conj_and(Columbia_NNP_19, Island_NNP_22); prep_in(established_VBN_12, greenhouses_NNS_27); conj_and(parts_NNS_14, greenhouses_NNS_27); amod(environments_NNS_31, other_JJ_29); amod(environments_NNS_31, protected_JJ_30); conj_and(parts_NNS_14, environments_NNS_31); conj_and(greenhouses_NNS_27, environments_NNS_31); punct(mean_VBP_7, ._._32)")
+
+    pattern.toString should {
+      " have two matches" in {
+        pattern(dgraph.graph).size must_== 2
+      }
+    }
+  }
+
+  def testPatternSerialization = {
+    val patterns = List(
+      """{arg1} <nsubj< {rel} >prep_of> {arg2}""",
+      """{arg1} <nsubj< {rel} >dobj> {arg2}""",
+      """{arg1} <nsubjpass< {slot0} >dep> {rel} >advmod> {arg2}""",
+      """{arg1} <nsubj< {rel} >prep_to> {arg2}""",
+      """{arg1} >appos> {rel} >prep_of> {arg2}""",
+      """{arg1} <nsubjpass< {rel} >prep_in> {arg2}""",
+      """{arg1} <nsubj< {rel} >prep_in> {arg2}""",
+      """{arg1} <prep_of< {rel} >prep_of> {arg2}""",
+      """{arg1} >conj_and> {rel} >prep_of> {arg2}""",
+      """{arg1} <nsubj< {rel} >prep_at> {arg2}""",
+      """{arg1} <nsubj< {arg2} >cop> {rel}""",
+      """{arg1} >partmod> {rel} >prep_in> {arg2}""",
+      """{arg1} <nsubjpass< {rel} >dep> {arg2}""",
+      """{arg1} <nsubj< {rel} >nsubj> {arg2}""",
+      """{arg1} <nsubj< {rel} >dobj> {slot0} >prep_of> {arg2}""",
+      """{arg1} <dobj< {rel} >nsubj> {arg2}""",
+      """{arg1} <conj_and< {arg2} <prep_of< {rel}""",
+      """{arg1} <nsubj< {rel} >prep_on> {arg2}""",
+      """{arg1} <nsubj< {rel} >dobj> {slot0} >conj_and> {arg2}""",
+      """{arg1} <prep_on< {rel} >prep_on> {arg2}""")
+    
+    for (patternString <- patterns) {
+      patternString should {
+        "deserialize correctly" in {
+          DependencyPattern.deserialize(patternString).toString must_== patternString
+        }
+      }
+    }
+  }
+
+  testPatternSerialization
+  testMultipathPatternApplication
+  
+  "patterns differ" in {
+    DependencyPattern.deserialize("{rel:postag=NNP:regex=president|son} <nn< {arg1} >nn> {arg2}") must_!= DependencyPattern.deserialize("{rel:postag=NN:regex=airline|author|book|brother|candidate|capital|ceo|chair|chairman|champion|chief|city|clone|coach|cofounder|commissioner|consort|cousin|creator|critic|daughter|dean|dictator|director|drummer|economist|editor|emperor|father|founder|friend|god|goddess|governor|graduate|head|home|host|husband|inventor|island|journal|king|lady|language|leader|man|manager|mascot|mayor|member|minister|moon|mother|name|native|newspaper|owner|part|pastor|people|premier|president|prophet|publication|publisher|queen|secretary|sign|son|speaker|star|state|student|subsidiary|suburb|unit|veteran|widow|wife|winner} <nn< {arg1} >nn> {arg2}")
+  }
+}
---
+++ sentence/piao/src/main/scala/tool/sentence/PiaoSentencer.scala
@@ -0,0 +1,18 @@
+package edu.washington.cs.knowitall
+package tool
+package sentence
+
+import spiaotools.SentParDetector
+
+/** A heuristic-based sentencer. */
+class PiaoSentencer() extends Sentencer {
+  val sentencer = new SentParDetector
+  def sentences(document: String) = 
+    sentencer.markupRawText(2, document).split("\\n+")
+}
+
+object PiaoSentencer
+extends SentencerMain {
+  lazy val sentencer = new PiaoSentencer
+}
+
