5de7d8bc2aaf2f560a970510bf276c7bbeb3b409
Michael Schmitz <michael@schmitztech.com>
2012-03-29 19:16:16 -0700

    Correctly handle empty strings with Morpha stemmer.

--- core/src/main/scala/tool/stem/MorphaStemmer.scala
+++ core/src/main/scala/tool/stem/MorphaStemmer.scala
@@ -11,23 +11,28 @@ class MorphaStemmer extends Stemmer {
   def stem(word: String) = this.morpha(word)
 
   def morpha(text: String) = {
-    val textParts = text.split(" ")
+    if (text.isEmpty) {
+      ""
+    }
+    else {
+      val textParts = text.split(" ")
 
-    val result = new StringBuilder
-    try {
-      for (i <- 0 until textParts.length) {
-        val morpha = new Morpha(new StringReader(textParts(i)), false)
-        result.append((if (result.length() == 0) "" else " ") + morpha.next())
+      val result = new StringBuilder
+      try {
+        for (i <- 0 until textParts.length) {
+          val morpha = new Morpha(new StringReader(textParts(i)), false)
+          result.append((if (result.length() == 0) "" else " ") + morpha.next())
+        }
       }
-    }
-    // yes, Morpha is cool enough to throw Errors
-    // usually when the text contains underscores
-    catch {
-      case e: Error => text
-      case e: java.io.IOException => text
-    }
+      // yes, Morpha is cool enough to throw Errors
+      // usually when the text contains underscores
+      catch {
+        case e: Error => text
+        case e: java.io.IOException => text
+      }
 
-    result.toString
+      result.toString
+    }
   }
 }
 
