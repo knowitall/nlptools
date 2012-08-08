85706c6f0d7adfd64abe6fd7f84bd7118995dfdb
Michael Schmitz <michael@schmitztech.com>
2012-08-08 10:59:14 -0700

    MorphaStemmer can now take advantages of postags.

--- stem/morpha/src/main/scala/tool/stem/MorphaStemmer.scala
+++ stem/morpha/src/main/scala/tool/stem/MorphaStemmer.scala
@@ -4,37 +4,15 @@ package stem
 
 import java.io.StringReader
 
+import edu.washington.cs.knowitall.morpha.{ MorphaStemmer => MorphaStem }
 import uk.ac.susx.informatics.Morpha
 import common.main.LineProcessor
 
 /** This stemmer handles many cases, but the JFlex is 5 MB. */
 class MorphaStemmer extends Stemmer {
-  def stem(word: String) = this.morpha(word)
+  def stem(word: String) = MorphaStem.stemToken(word)
 
-  def morpha(text: String) = {
-    if (text.isEmpty) {
-      ""
-    }
-    else {
-      val textParts = text.split(" ")
-
-      val result = new StringBuilder
-      try {
-        for (i <- 0 until textParts.length) {
-          val morpha = new Morpha(new StringReader(textParts(i)), false)
-          result.append((if (result.length() == 0) "" else " ") + morpha.next())
-        }
-      }
-      // yes, Morpha is cool enough to throw Errors
-      // usually when the text contains underscores
-      catch {
-        case e: Error => text
-        case e: java.io.IOException => text
-      }
-
-      result.toString
-    }
-  }
+  override def stem(word: String, postag: String) = MorphaStem.stemToken(word, postag)
 }
 
 object MorphaStemmer
