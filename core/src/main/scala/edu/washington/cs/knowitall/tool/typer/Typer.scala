5212cde55e5cf0e39741d6da7fcba37a73cf97cf
Michael Schmitz <michael@schmitztech.com>
2012-10-04 13:07:18 -0700

    First pass on typer system.

---
+++ core/src/main/scala/edu/washington/cs/knowitall/tool/typer/Typer.scala
@@ -0,0 +1,14 @@
+package edu.washington.cs.knowitall.tool.typer
+
+import edu.washington.cs.knowitall.collection.immutable.Interval
+import edu.washington.cs.knowitall.tool.tokenize.Token
+
+abstract class Typer[E <: Token](val name: String, val source: String) {
+  def apply(seq: Seq[E]): Seq[Type]
+}
+
+case class Type(val name: String, val source: String, val interval: Interval, val text: String) {
+  def matchText[E <: Token](seq: Seq[E]): String = seq.iterator.slice(interval.start, interval.end).map(_.string).mkString(" ")
+
+  def tokens[E <: Token](seq: Seq[E]): Seq[String] = seq.slice(interval.start, interval.end)
+}
