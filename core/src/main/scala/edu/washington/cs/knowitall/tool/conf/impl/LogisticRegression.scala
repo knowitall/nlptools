54758f88af89227cec70f7cf4bd6e7ffb48a7898
Michael Schmitz <michael@schmitztech.com>
2012-10-16 13:23:21 -0700

    Fix on logistic regression save.

--- core/src/main/scala/edu/washington/cs/knowitall/tool/conf/impl/LogisticRegression.scala
+++ core/src/main/scala/edu/washington/cs/knowitall/tool/conf/impl/LogisticRegression.scala
@@ -9,8 +9,8 @@ import edu.washington.cs.knowitall.common.Resource.using
 import java.io.OutputStream
 
 /** An implementation of logistic regression of features that can be
-  * represented as a double. 
-  * 
+  * represented as a double.
+  *
   * @param  featureSet  the features to use
   * @param  featureWeights  the feature weights
   * @param  intercept  the intercept value
@@ -21,7 +21,7 @@ class LogisticRegression[T](
   val intercept: Double) extends ConfidenceFunction[T](featureSet) {
 
   def this(featureSet: FeatureSet[T, Double], weights: Map[String, Double]) = {
-    this(featureSet, weights, weights.getOrElse("Intercept", 0.0))
+    this(featureSet, weights - "Intercept", weights.getOrElse("Intercept", 0.0))
   }
 
   override def apply(extraction: T): Double = getConf(extraction)
@@ -40,7 +40,7 @@ class LogisticRegression[T](
 
     1.0 / (1.0 + math.exp(-(exponentCoefficient*z + this.intercept)))
   }
-  
+
   override def save(output: OutputStream): Unit = {
     using (new PrintWriter(output)) { pw =>
       save(pw)
@@ -52,6 +52,6 @@ class LogisticRegression[T](
       writer.println(name + "\t" + weight)
     }
 
-    println("Intercept" + "\t" + intercept)
+    writer.println("Intercept" + "\t" + intercept)
   }
 }
