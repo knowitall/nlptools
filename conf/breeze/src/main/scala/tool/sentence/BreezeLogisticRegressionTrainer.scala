be33f7040bc231b8d14f93e5a7f4e8a66df11027
Michael Schmitz <michael@schmitztech.com>
2012-10-16 17:39:17 -0700

    Added the ability to specify optional parameters to breeze trainer.

--- conf/breeze/src/main/scala/tool/sentence/BreezeLogisticRegressionTrainer.scala
+++ conf/breeze/src/main/scala/tool/sentence/BreezeLogisticRegressionTrainer.scala
@@ -9,7 +9,7 @@ import breeze.optimize.FirstOrderMinimizer.OptParams
 import edu.washington.cs.knowitall.tool.conf.impl.LogisticRegression
 
 class BreezeLogisticRegressionTrainer[E](features: FeatureSet[E, Double]) extends ConfidenceTrainer[E](features) {
-  def trainBreezeClassifier(instances: Iterable[Labelled[E]], optParams: OptParams = OptParams(useL1 = true)) = {
+  def trainBreezeClassifier(instances: Iterable[Labelled[E]], optParams: OptParams) = {
     val examples = instances.zipWithIndex map { case (Labelled(label, item: E), i) =>
       val vector = DenseVector((1.0 +: features.vectorize(item)).toArray)
       Example[Boolean, DenseVector[Double]](label, vector, id=i.toString)
@@ -17,11 +17,15 @@ class BreezeLogisticRegressionTrainer[E](features: FeatureSet[E, Double]) extend
 
     new LogisticClassifier.Trainer[Boolean,DenseVector[Double]](optParams).train(examples)
   }
-
-  override def train(labelled: Iterable[Labelled[E]]): LogisticRegression[E] = {
-    val classifier = trainBreezeClassifier(labelled)
+  
+  def train(labelled: Iterable[Labelled[E]], optParams: OptParams): LogisticRegression[E] = {
+    val classifier = trainBreezeClassifier(labelled, optParams)
 
     val weights = (("Intercept" +: features.featureNames).iterator zip classifier.featureWeights.indexed(true).iterator.map(_._2)).toMap
     new LogisticRegression(features, weights, 0.0)
+  }
+
+  override def train(labelled: Iterable[Labelled[E]]): LogisticRegression[E] = {
+    train(labelled, OptParams(useL1 = true))
   }
 }
