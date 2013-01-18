74c2899dbe7b2d19e521b8372d53d544076cb534
Michael Schmitz <michael@schmitztech.com>
2013-01-18 15:41:05 -0800

    Added two possessives.

--- core/src/main/scala/edu/washington/cs/knowitall/tool/postag/Postagger.scala
+++ core/src/main/scala/edu/washington/cs/knowitall/tool/postag/Postagger.scala
@@ -25,11 +25,11 @@ object Postagger {
   val subjectPronouns = immutable.Set("i", "you", "he", "she", "it", "they", "we")
   val objectPronouns = immutable.Set("me", "you", "him", "her", "it", "us", "them")
   val reflexivePronouns = immutable.Set("myself", "yourself", "himself", "itself", "themself", "ourselves", "yourselves", "themselves")
-  val possessivePronouns = immutable.Set("mine", "yours", "his", "hers", "its", "ours", "theirs")
+  val possessivePronouns = immutable.Set("mine", "yours", "his", "hers", "its", "ours", "theirs", "whose")
   val pronouns = subjectPronouns ++ objectPronouns ++ reflexivePronouns ++ possessivePronouns
 
   val articles = immutable.Set("a", "an", "the")
-  val possessives = immutable.Set("my", "your", "his", "her", "its", "our", "your", "their")
+  val possessives = immutable.Set("my", "your", "his", "her", "its", "one's", "our", "your", "their")
 
   val simplePrepositions = immutable.Set(
     "a", "abaft", "aboard", "about", "above", "absent", "across", "afore",
