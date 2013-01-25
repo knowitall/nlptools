37427b0ea4411e42553f3d9ea65af0b722855e59
Michael Schmitz <michael@schmitztech.com>
2013-01-25 09:47:03 -0800

    Fix blipp test.

--- parse/bllip/src/test/scala/tool/parse/BllipParserSpecTest.scala
+++ parse/bllip/src/test/scala/tool/parse/BllipParserSpecTest.scala
@@ -20,8 +20,8 @@ object BllipParserSpecTest extends Specification {
     val text = "This is a test of the Stanford Parser."
     val parser = new BllipParser
 
-    val dependency = parser.dependencyGraph(text, BaseStanfordParser.CCCompressed)
-    dependency.toString must_== "(of_IN_4_15); (._._8_37); nsubj(test_NN_3_10, This_DT_0_0); cop(test_NN_3_10, is_VBZ_1_5); det(test_NN_3_10, a_DT_2_8); prep_of(test_NN_3_10, Parser_NNP_7_31); det(Parser_NNP_7_31, the_DT_5_18); nn(Parser_NNP_7_31, Stanford_NNP_6_22)"
+    val dependency = parser.dependencyGraph(text, BaseStanfordParser.None)
+    dependency.toString must_== "nsubj(test_NN_3_10, This_DT_0_0); cop(test_NN_3_10, is_AUX_1_5); det(test_NN_3_10, a_DT_2_8); prep(test_NN_3_10, of_IN_4_15); pobj(of_IN_4_15, Parser_NNP_7_31); det(Parser_NNP_7_31, the_DT_5_18); nn(Parser_NNP_7_31, Stanford_NNP_6_22)"
   }
 }
 
