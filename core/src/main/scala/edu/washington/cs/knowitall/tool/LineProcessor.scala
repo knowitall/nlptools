43b8214ea6fc2c0f4d3c04af1e5f14a1f02e1e2f
Michael Schmitz <michael@schmitztech.com>
2013-02-04 09:25:25 -0800

    Added input/output file support for line processor.

--- core/src/main/scala/edu/washington/cs/knowitall/tool/LineProcessor.scala
+++ core/src/main/scala/edu/washington/cs/knowitall/tool/LineProcessor.scala
@@ -4,20 +4,22 @@ import unfiltered.request._
 import unfiltered.response._
 import unfiltered.filter.Planify
 import edu.washington.cs.knowitall.common.Timing
+import java.io.File
+import java.util.Scanner
+import java.io.PrintWriter
 
 abstract class LineProcessor(name: String) {
   import scopt.immutable._
   import scopt.immutable.OptionParser._
 
-  class Config(val server: Boolean = false, val port: Int = 8080) {
-    def copy(server: Boolean = server, port: Int = port) =
-      new Config(server, port)
-  }
+  case class Config(val server: Boolean = false, val port: Int = 8080, val outputFile: Option[File] = None, val inputFile: Option[File] = None)
 
   val parser = new scopt.immutable.OptionParser[Config](name) {
     def options = Seq(
       flag("server", "run as a server") { (c: Config) => c.copy(server = true) },
-      intOpt("port", "which port to run the server on") { (port: Int, c: Config) => c.copy(port = port) }
+      intOpt("port", "which port to run the server on") { (port: Int, c: Config) => c.copy(port = port) },
+      argOpt("input", "file to input from") { (path: String, c: Config) => c.copy(inputFile=Some(new File(path))) },
+      argOpt("output", "file to output to") { (path: String, c: Config) => c.copy(outputFile=Some(new File(path))) }
     )
   }
 
@@ -36,22 +38,33 @@ abstract class LineProcessor(name: String) {
     else runCli(config)
   }
 
-  def handle(line: String): Unit = {
-    println(process(line))
+  def handle(writer: PrintWriter, line: String): Unit = {
+    writer.println(process(line))
   }
 
   def process(line: String): String
 
   def runCli(config: Config) {
-    val scanner = new java.util.Scanner(System.in, "UTF-8")
+    val scanner = config.inputFile match {
+      case Some(file) => new Scanner(file, "UTF-8")
+      case None => new Scanner(System.in, "UTF-8")
+    }
+
+    val writer = config.outputFile match {
+      case Some(file) => new PrintWriter(file, "UTF-8")
+      case None => new PrintWriter(System.out)
+    }
 
     val ns = Timing.time {
       while (scanner.hasNextLine) {
-        handle(scanner.nextLine)
+        handle(writer, scanner.nextLine)
       }
     }
 
     System.err.println(Timing.Seconds.format(ns))
+
+    scanner.close()
+    writer.close()
   }
 
   def runServer(config: Config) = {
