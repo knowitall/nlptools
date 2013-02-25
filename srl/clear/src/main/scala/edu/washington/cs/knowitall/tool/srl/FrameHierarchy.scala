af421195e7952a7efdf55c3770e5d5617e7183b6
Michael Schmitz <michael@schmitztech.com>
2013-02-25 14:57:05 -0800

    Handle empty case in FrameHierarchy.

--- srl/clear/src/main/scala/edu/washington/cs/knowitall/tool/srl/FrameHierarchy.scala
+++ srl/clear/src/main/scala/edu/washington/cs/knowitall/tool/srl/FrameHierarchy.scala
@@ -8,34 +8,39 @@ case class FrameHierarchy(frame: Frame, children: Seq[FrameHierarchy]) {
 
 object FrameHierarchy {
   def fromFrames(dgraph: DependencyGraph, frames: IndexedSeq[Frame]) = {
-    val framesWithIndex = frames.zipWithIndex
+    if (frames.isEmpty) Seq.empty
+    else {
+      val framesWithIndex = frames.zipWithIndex
 
-    // find all ancestor -> descendant relationships
-    val descendants = framesWithIndex.map { case (frame, index) =>
-      val inferiors = dgraph.graph.inferiors(frame.relation.node)
-      val children = framesWithIndex.filter { case (child, index) => 
-        frame != child && 
-        // first arguments must match
-        frame.argument(Roles.A0) == child.argument(Roles.A0) &&
-        // child frame must be beneath parent frame relation in dependency graph
-        (inferiors contains child.relation.node) 
+      // find all ancestor -> descendant relationships
+      val descendants = framesWithIndex.map {
+        case (frame, index) =>
+          val inferiors = dgraph.graph.inferiors(frame.relation.node)
+          val children = framesWithIndex.filter {
+            case (child, index) =>
+              frame != child &&
+                // first arguments must match
+                frame.argument(Roles.A0) == child.argument(Roles.A0) &&
+                // child frame must be beneath parent frame relation in dependency graph
+                (inferiors contains child.relation.node)
+          }
+          index -> children.map(_._2)
       }
-      index -> children.map(_._2)
-    }
 
-    var hierarchies = Map.empty[Int, FrameHierarchy]
-    for (i <- Range(0, descendants.iterator.map(_._2.size).max + 1)) {
-      val targetDescendants = descendants.filter(_._2.size == i)
+      var hierarchies = Map.empty[Int, FrameHierarchy]
+      for (i <- Range(0, descendants.iterator.map(_._2.size).max + 1)) {
+        val targetDescendants = descendants.filter(_._2.size == i)
 
-      for ((frameIndex, childrenIndices) <- targetDescendants) {
-        val frame = frames(frameIndex)
-        val children = childrenIndices map hierarchies
-        hierarchies --= childrenIndices
+        for ((frameIndex, childrenIndices) <- targetDescendants) {
+          val frame = frames(frameIndex)
+          val children = childrenIndices map hierarchies
+          hierarchies --= childrenIndices
 
-        hierarchies += frameIndex -> FrameHierarchy(frame, children)
+          hierarchies += frameIndex -> FrameHierarchy(frame, children)
+        }
       }
-    }
 
-    hierarchies.map(_._2)
+      hierarchies.map(_._2)
+    }
   }
 }
