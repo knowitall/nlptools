00777ea86206e26f3e2d16ece5b0283f26bb33b6
Michael Schmitz <michael@schmitztech.com>
2013-02-25 14:22:18 -0800

    Change case of SRL to Srl.

--- srl/clear/src/main/scala/edu/washington/cs/knowitall/tool/srl/SRL.scala
+++ srl/clear/src/main/scala/edu/washington/cs/knowitall/tool/srl/SRL.scala
@@ -3,6 +3,6 @@ package edu.washington.cs.knowitall.tool.srl
 import edu.washington.cs.knowitall.tool.tokenize.Token
 import edu.washington.cs.knowitall.tool.parse.graph.DependencyGraph
 
-abstract class SRL {
+abstract class Srl {
   def apply(graph: DependencyGraph): Seq[Frame]
 }
