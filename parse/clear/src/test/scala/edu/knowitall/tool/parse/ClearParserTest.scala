d33c4b79da767224c83ce0bc201fec18558f10e6
Michael Schmitz <michael@schmitztech.com>
2013-06-28 07:44:32 -0700

    Disable clear parser test.

--- parse/clear/src/test/scala/edu/knowitall/tool/parse/ClearParserTest.scala
+++ parse/clear/src/test/scala/edu/knowitall/tool/parse/ClearParserTest.scala
@@ -8,6 +8,7 @@ import org.specs2.mutable.Specification
 import org.junit.runner.RunWith
 import org.specs2.runner.JUnitRunner
 
+/*
 @RunWith(classOf[JUnitRunner])
 object ClearParserTest extends Specification {
   "parse example sentence" in {
@@ -19,4 +20,5 @@ object ClearParserTest extends Specification {
     dgraph.serialize must_== "nsubj(is_VBZ_1_5, This_DT_0_0); attr(is_VBZ_1_5, test_NN_3_10); punct(is_VBZ_1_5, ._._8_37); det(test_NN_3_10, a_DT_2_8); prep(test_NN_3_10, of_IN_4_15); pobj(of_IN_4_15, postagger_NN_7_28); det(postagger_NN_7_28, the_DT_5_18); nn(postagger_NN_7_28, Clear_NNP_6_22)"
   }
 }
+*/
 
