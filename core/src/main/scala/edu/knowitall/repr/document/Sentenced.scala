363d9af71fc72c1ea3bddb70cb5545318f49a71e
Rob Bart <rbart@cs.washington.edu>
2013-10-17 12:10:39 -0700

    Break Sentenced apart into Sentencer and Sentenced

--- core/src/main/scala/edu/knowitall/repr/document/Sentenced.scala
+++ core/src/main/scala/edu/knowitall/repr/document/Sentenced.scala
@@ -8,10 +8,16 @@ case class DocumentSentence[S <: Sentence](sentence: S, offset: Int)
 trait Sentenced[S <: Sentence] {
   this: Document =>
 
+  def sentences: Stream[DocumentSentence[S]]
+}
+
+trait Sentencer[S <: Sentence] extends Sentenced[S] {
+  this: Document =>
+
   def constructor(text: String): S
   def sentencer: Segmenter
 
-  lazy val sentences: Stream[DocumentSentence[S]] =
+  override lazy val sentences: Stream[DocumentSentence[S]] =
     sentencer(text).toStream.map { segment =>
       DocumentSentence(constructor(segment.text), segment.offset)
     }
