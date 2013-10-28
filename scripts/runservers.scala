97cfa54b15a3d1cd2dc4c522cb72edf1b269d74d
Michael Schmitz <michael@schmitztech.com>
2013-10-28 17:43:20 +0000

    Run servers async.

--- scripts/runservers.scala
+++ scripts/runservers.scala
@@ -4,10 +4,17 @@ val portFile = args(0)
 
 println("Server tools specified in: " + portFile)
 
+var processes = List.empty[(Process, String)]
 for (line <- io.Source.fromFile(portFile).getLines) {
   val Array(path, port, jar) = line.split("\t")
 
   print(s"Serving $path at $port ($jar): ")
-  val ec = Seq("java", "-jar", jar, "--server", "--port", port).!
-  println(ec)
+  val pc = Seq("java", "-jar", jar, "--server", "--port", port).run
+  processes ::= (pc, path)
+}
+
+println("Waiting for processes to exit: ")
+for ((pc, path) <- processes) {
+  val ec = pc.exitValue()
+  println(ec + ":\t" + path)
 }
