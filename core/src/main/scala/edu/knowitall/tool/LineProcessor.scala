b465a3069b0114194be7fb53c1672a2511f9f6f5
Michael Schmitz <michael@schmitztech.com>
2013-10-30 12:58:51 -0700

    Require port to be specified.

--- core/src/main/scala/edu/knowitall/tool/LineProcessor.scala
+++ core/src/main/scala/edu/knowitall/tool/LineProcessor.scala
@@ -18,7 +18,9 @@ abstract class LineProcessor(name: String) {
   val parser = new scopt.immutable.OptionParser[Config](name) {
     def options = Seq(
       flag("server", "run as a server") { (c: Config) => c.copy(server = true) },
-      intOpt("port", "which port to run the server on") { (port: Int, c: Config) => c.copy(port = port) },
+      intOpt("port", "which port to run the server on") { (port: Int, c: Config) =>
+        require(c.server, "--server must be set with --port"); c.copy(port = port)
+      },
       argOpt("input", "file to input from") { (path: String, c: Config) => c.copy(inputFile=Some(new File(path))) },
       argOpt("output", "file to output to") { (path: String, c: Config) => c.copy(outputFile=Some(new File(path))) },
       flag("parallel", "parallel execution") { (c: Config) => c.copy(parallel=true) }
