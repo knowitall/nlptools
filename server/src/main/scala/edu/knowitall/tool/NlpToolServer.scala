2ee7637d6bb506600a3f7e3111e0430ed75ad887
Michael Schmitz <michael@schmitztech.com>
2013-11-05 23:33:56 +0000

    Add logging option.

--- server/src/main/scala/edu/knowitall/tool/NlpToolServer.scala
+++ server/src/main/scala/edu/knowitall/tool/NlpToolServer.scala
@@ -5,11 +5,14 @@ import dispatch._
 import Defaults._
 
 object NlpToolServer extends App {
-  case class Config(configFile: File = null, port: Int = 8080)
+  case class Config(configFile: File = null, logPath: Option[File] = None, port: Int = 8080)
 
   val parser = new scopt.OptionParser[Config]("scopt") {
     arg[File]("<file>") action { (x, c) =>
       c.copy(configFile = x)
+    }
+    opt[File]('l', "logs") action { (x, c) =>
+      c.copy(logPath = Some(x))
     }
     opt[Int]('p', "port") action { (x, c) =>
       c.copy(port = x)
@@ -17,7 +20,7 @@ object NlpToolServer extends App {
   }
 
   parser.parse(args, Config()).foreach { config =>
-    val server = new NlpToolServer(config.port, readTools(config.configFile))
+    val server = new NlpToolServer(config, readTools(config.configFile))
     server.run()
   }
 
@@ -43,7 +46,7 @@ case class Tool(path: String, port: Int) {
     else this
   }
 }
-class NlpToolServer(port: Int, tools: Seq[Tool]) {
+class NlpToolServer(config: NlpToolServer.Config, tools: Seq[Tool]) {
   val toolMap = tools.map(_.withoutTrailingSlash).groupBy(_.path).map { case (k, v) =>
     (k, v.head)
   }
@@ -55,6 +58,7 @@ class NlpToolServer(port: Int, tools: Seq[Tool]) {
   def run() {
     def dropSlash(s: String) = if (s endsWith "/") s.dropRight(1) else s
     val plan = Planify {
+      // GET status as HTML
       case GET(Path("/")) =>
         ResponseString(
           """<html><head><title>Http NlpTools Status</title></head><body>""" +
@@ -68,11 +72,27 @@ class NlpToolServer(port: Int, tools: Seq[Tool]) {
           }.mkString("<br/>\n") +
           """</html>"""
         )
+
+      // GET status as plain text
       case GET(Path("/plaintext")) =>
         ResponseString(tools.map(_.path).mkString("\n"))
+
       case GET(Path(path)) if toolMap.contains(dropSlash(path)) =>
         val tool = toolMap(dropSlash(path))
         ResponseString(Http(tool.svc OK as.String).apply())
+
+      // GET tool logs
+      case GET(Path(Seg(tool :: impl :: ext :: Nil))) 
+        if toolMap.contains(s"/$tool/$impl") && 
+           config.logPath.isDefined =>
+          val filename = s"$tool-$impl.$ext"
+          val file = new File(config.logPath.get, filename)
+          if (file.exists()) {
+            ResponseString(io.Source.fromFile(file).getLines.mkString("\n"))
+          }
+          else {
+            ResponseString("File does not exists: " + file)
+          }
       case req @ POST(Path(path)) if toolMap.contains(dropSlash(path)) =>
         val tool = toolMap(dropSlash(path))
         val payload = Body.string(req)
@@ -81,7 +101,7 @@ class NlpToolServer(port: Int, tools: Seq[Tool]) {
       case req @ POST(Path(path)) => ResponseString("Tool not found in POST: " + path)
     }
 
-    unfiltered.jetty.Http(port).filter(plan).run()
-    System.out.println("Server started on port: " + port);
+    unfiltered.jetty.Http(config.port).filter(plan).run()
+    System.out.println("Server started on port: " + config.port)
   }
 }
