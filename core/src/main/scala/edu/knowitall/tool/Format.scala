26a4c202a2155768e26081ce1ac0784190b403fe
Michael Schmitz <michael@schmitztech.com>
2013-11-06 16:31:14 -0800

    Add readTry method.

--- core/src/main/scala/edu/knowitall/tool/Format.scala
+++ core/src/main/scala/edu/knowitall/tool/Format.scala
@@ -1,11 +1,14 @@
 package edu.knowitall.tool
 
+import scala.util.Try
+
 trait Writer[F, T] {
   def write(from: F): T
 }
 
 trait Reader[F, T] {
   def read(from: F): T
+  def readTry(from: F): Try[T] = Try(this.read(from))
 }
 
 trait Format[F, T]
