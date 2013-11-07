4e90aa937c556f705d140596fd87c1cefd16d3a6
Michael Schmitz <michael@schmitztech.com>
2013-11-06 16:32:08 -0800

    Post single line dgraph for SRL remote.

--- core/src/main/scala/edu/knowitall/tool/srl/RemoteSrl.scala
+++ core/src/main/scala/edu/knowitall/tool/srl/RemoteSrl.scala
@@ -5,7 +5,7 @@ import edu.knowitall.tool.parse.graph._
 
 class RemoteSrl(val urlString: String) extends Srl with Remote {
   def apply(dgraph: DependencyGraph) = {
-    val response = this.post(urlString)
+    val response = this.post(DependencyGraph.stringFormat.write(dgraph))
     if (response.isEmpty) Seq.empty
     else {
       response.split("\\n").map(Frame.deserialize(dgraph))(scala.collection.breakOut)
