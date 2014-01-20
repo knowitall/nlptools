4607cfba648e8c5138d28e6281824732bb484090
Michael Schmitz <MichaelS@vulcan.com>
2014-01-20 23:38:10 +0000

    Fixups

--- scripts/assignports.scala
+++ scripts/assignports.scala
@@ -4,6 +4,6 @@ val assemblies = Seq("find", "-name", "*assembly*jar").lines
 var port = 12001
 for (assembly <- assemblies) {
   val Array(_, tool, system, _ @ _*) = assembly.split("/")
-  println(s"/$tool/$system/\t$port\t$assembly")
+  println(s"/$tool/$system/\thttp://localhost:$port/\t$assembly")
   port = port + 1
 }
